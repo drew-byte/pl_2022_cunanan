b9e4eddcb8f6ba25f3f7273a5f5fec1253f0ac48
Andrew <82567656+drew-byte@users.noreply.github.com>
2022-09-28 00:22:18 +0800

    Activity3

---
+++ Activity3.java
@@ -0,0 +1,52 @@
+
+import java.util.Scanner;
+
+class Activity3 {
+    public static void main(String[] args) {
+        method();
+
+    }
+
+    public static void printer(String p) {
+        System.out.print(p);
+    }
+
+    public static void method() {
+        Scanner sc = new Scanner(System.in);
+        printer("Enter Source Language: ");
+        String i = sc.nextLine();
+
+        printer("Output is: ");
+        String input[] = i.split(" ", 5);
+        String choices[] = { "String", "int", "char", "double" };
+        int val = 0;
+        for (String string : input) {
+
+            if (string.contains(choices[0])) {
+                printer("<data_type>");
+            } else if (string.contains(choices[1])) {
+                printer("<data_type>");
+            } else if (string.contains(choices[2])) {
+                printer("<data_type>");
+            } else if (string.contains(choices[3])) {
+                printer("<data_type>");
+            } else if (string.contains("=")) {
+                printer("<assignment_operator>");
+            } else if (string.contains(";")) {
+                if (val == 0 && string.contains("\"") || string.contains(";")
+                        || string.contains("'")) {
+                    printer("<value><delimiter>");
+                } else if (string.contains(";")) {
+                    printer("<delimiter>");
+                }
+            } else if (val == 0 && string.contains("\"")
+                    || string.contains("'")) {
+                val++;
+                printer("<value>");
+            } else {
+                printer("<identifier>");
+            }
+
+        }
+    }
+}
